bd1cf73fde4f1814e55da09e2513a5cfdcb418b4
Your Name <you@example.com>
2019-10-02 19:21:00 +0200

    Added the routhe paths for each component

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const Cities = () => {
 
     return (
-        <div >
+        <div  >
             <p>Travel Cities</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam inventore at excepturi fuga enim, magnam doloremque saepe adipisci et commodi aut similique dolorum incidunt repudiandae. Pariatur nisi explicabo nesciunt neque!</p>
 
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MYtineraryLogo from '../MYtineraryLogo.png'
 import homeIcon from '../homeIcon.png'
 import circledRightarrow from '../circledRightarrow.png'
+import { NavLink } from 'react-router-dom';
+
 
 export class LandingPage extends React.Component {
 
@@ -13,13 +15,24 @@ export class LandingPage extends React.Component {
                     <p className="itinerarydescription">Find your perfect trip, designed by insiders who know and love their cities.</p>
                 </div>
 
-                <div className="mid-container">
-                    <p className="itinerarydescription">Start Browsing</p>
-                    <img src={circledRightarrow} alt="RightArrow" />
+                <div className="mid-container" >
+                    <NavLink to="/Cities">
+                        <p className="itinerarydescription" >Start Browsing</p>
+                        <img src={circledRightarrow} alt="RightArrow" />
+                    </NavLink>
+                </div>
+
+
+
+                <div className="Login">
+                    <NavLink to="./SignUp">SignUp</NavLink>
+                    <NavLink to="./LogIn">LogIn</NavLink>
+
                 </div>
 
                 <div className="footer-container">
                     <p className="itinerarydescription">Want to build your own MYtinerary ?</p>
+
                     <footer>
                         <img src={homeIcon} alt="homeIcon" />
                     </footer>
