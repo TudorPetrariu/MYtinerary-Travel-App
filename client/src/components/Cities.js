e75dfaa614ce3734a8327bac621280150dc594a6
Tudor <tudor.petrariu@yahoo.com>
2019-10-09 10:34:11 +0200

    fetched data from Mongo.DB and render it to Cities page

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -1,29 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { HomeButton } from './HomeButton'
 
-const Cities = () => {
 
-    return (
-        <div className="app">
-            <div className="header-container">
-                <p className="itinerarydescription">Find your perfect city</p>
+class MyCities extends Component {
+
+    state = {
+        cities: []
+    }
+
+
+    async componentDidMount() {
+
+        let cities = await fetch('/cities', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+            .then(res => res.json())
+            .then(json => json.data)
+
+        this.setState({
+            cities: cities
+        })
+
+
+    }
+
+    render() {
+        console.log(this.state.cities)
+        const { cities } = this.state;
+        const citiesList = cities.length ? (cities.map(city => {
+            return (
+                <div key={city._id}>
+                    <div>{city.name}</div>
+                    <div>{city.country}</div>
+                    <img src={city.images} alt="cityimages"></img>
+
+                </div>
+            )
+        })) : (
+                <div>nothing</div>
+            )
+        return (
+            <div className="app">
+                <div className="header-container">
+                    <p className="itinerarydescription">Find your perfect city</p>
+                </div>
+
+                <div className="mid-container" >
+                    <p className="itinerarydescription" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quas commodi tenetur esse nemo nesciunt, corporis praesentium deserunt. Modi facilis corrupti assumenda ipsum libero neque veritatis exercitationem voluptatem quas possimus?</p>
+                </div>
+                {citiesList}
+                <div className="footer-container">
+                    <p className="itinerarydescription">Search</p>
+
+                    <HomeButton />
+
+                </div>
             </div>
 
-            <div className="mid-container" >
-                <p className="itinerarydescription" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quas commodi tenetur esse nemo nesciunt, corporis praesentium deserunt. Modi facilis corrupti assumenda ipsum libero neque veritatis exercitationem voluptatem quas possimus?</p>
-            </div>
-
-
-
-
-
-            <div className="footer-container">
-                <p className="itinerarydescription">Search</p>
-
-                <HomeButton />
-
-            </div>
-        </div>
-    )
+        )
+    }
 }
-export default Cities;
+export default MyCities;
