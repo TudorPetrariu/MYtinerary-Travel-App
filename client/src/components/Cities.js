806d013d62eb788cadd29589ff0e99edbd3108dc
TudorPetrariu <tudor.petrariu@yahoo.com>
2019-10-14 16:47:46 +0200

    Implemented Search Filter for Cities component and added Materialize CSS to the application

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -7,35 +7,57 @@ import { fetchCities } from "../redux/actions/cityAction"
 
 
 class MyCities extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            cities: '',
+            word: '',
+        }
 
+        this.searchHandler = this.searchHandler.bind(this)
+    }
 
+    searchHandler = (e) => {
+        e.preventDefault()
+        this.setState({
+            word: e.target.value
+        })
 
+    }
 
     componentDidMount() {
         this.props.getCities()
     }
 
     render() {
+
         console.log(this.props)
         const { cities } = this.props
-        const citiesList = cities.length ? (cities.map(city => {
+        const { word } = this.state
+        const citiesList = cities.length ? (cities.filter(search(word)).map(city => {
 
             return (
-                <div key={city._id}>
-                    <li> {city.name} </li>
-                    <img src={city.images} alt="cityimages"></img>
+
+                <div key={city._id} className="card center" >
+                    <span > {city.name}  </span>
+                    <img src={city.images} alt="cityimages" ></img>
                 </div>
             )
         })) : (
-                <div>Fetching Cities..</div>
+                <div className="center">Fetching Cities..</div>
             )
 
         return (
             <div>
+                <form>
+                    <label htmlFor="SearchBar" >Search your city</label>
+                    <input type="text" id="SearchBar" onChange={this.searchHandler} values={word}></input>
+                </form>
 
-                <div >
+                <div className=" container ">
                     {citiesList}
                 </div>
+
                 <div className="footer-container">
                     <HomeButton />
                 </div>
@@ -48,8 +70,12 @@ class MyCities extends Component {
 const mapStateToProps = (state) => {
     return {
 
-        cities: state.cities
+        cities: state.cities,
+        word: ''
+
+
     }
+
 }
 
 const mapDispatchToProps = (dispatch) => {
@@ -57,7 +83,11 @@ const mapDispatchToProps = (dispatch) => {
         getCities: () => dispatch(fetchCities())
     }
 }
-
+function search(word) {
+    return (x) => {
+        return x.name.toLowerCase().includes(word.toLowerCase()) || !word;
+    }
+}
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyCities);
